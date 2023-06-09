20fe33eda7a126cd6bde6c9216e9d07c96705c2a
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2023-06-08 21:38:35 -0400

    ajustement du css du header et ajout de section dans le front-page.php

--- themes/wp-la-presse/front-page.php
+++ themes/wp-la-presse/front-page.php
@@ -1,11 +1,18 @@
 <?php
 get_header();
 ?>
-<h1>Front-page</h1>
+<h1>LES GRANDS TITRES</h1>
 
 <?php 
-    while(have_posts()){
-        the_post();
+    //------------------------------------------------------plus recents articles
+    $nouveauxArticles = new WP_Query(array(
+        'posts_per_page' => 5,
+        'orderby' => 'date',
+        'order' => 'DESC',
+    ));
+
+    while($nouveauxArticles->have_posts()){
+        $nouveauxArticles->the_post();
         ?>
         <div class="article">
             <a href=<?php the_permalink(); ?>><?php the_post_thumbnail('medium'); ?></a>
@@ -26,6 +33,166 @@ get_header();
         </div>
         <?php
     }
+    //-----------------------------------------------------------plus recents articles international
+    $articlesInternational = new WP_Query(array(
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'category_name' => 'INTERNATIONAL'
+    ));
+    ?>
+    <h1>INTERNATIONAL</h1>
+    <?php
+    while($articlesInternational->have_posts()){
+        $articlesInternational->the_post();
+        ?>
+        <div class="article">
+            <a href=<?php the_permalink(); ?>><?php the_post_thumbnail('medium'); ?></a>
+            <div class="the-content">
+                <div class="<?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?>"> <?php echo get_the_category_list(', ') ?> </div>
+                <h2><a href=<?php the_permalink(); ?>> <?php the_title(); ?> </a></h2>
+                <div class = "extrait">
+                <?php
+                    if(has_excerpt()){
+                        the_excerpt();
+                    }else{
+                        echo wp_trim_words(get_the_content(), 25);
+                    }
+                ?>
+                </div>
+                <p class="temps"> <?php the_time('j F Y',) ?> </p>
+            </div>
+        </div>
+        <?php
+    }
+    //-----------------------------------------------------------plus recents chroniques
+    $articlesChroniques = new WP_Query(array(
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'category_name' => 'CHRONIQUES'
+    ));
+    ?>
+    <h1>CHRONIQUES</h1>
+    <?php
+    while($articlesChroniques->have_posts()){
+        $articlesChroniques->the_post();
+        ?>
+        <div class="article">
+            <a href=<?php the_permalink(); ?>><?php the_post_thumbnail('medium'); ?></a>
+            <div class="the-content">
+                <div class="<?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?>"> <?php echo get_the_category_list(', ') ?> </div>
+                <h2><a href=<?php the_permalink(); ?>> <?php the_title(); ?> </a></h2>
+                <div class = "extrait">
+                <?php
+                    if(has_excerpt()){
+                        the_excerpt();
+                    }else{
+                        echo wp_trim_words(get_the_content(), 25);
+                    }
+                ?>
+                </div>
+                <p class="temps"> <?php the_time('j F Y',) ?> </p>
+            </div>
+        </div>
+        <?php
+    }
+      //-----------------------------------------------------------plus recents arcticles environnement
+      $articlesEnvironnement = new WP_Query(array(
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'category_name' => 'ENVIRONNEMENT'
+    ));
+    ?>
+    <h1>ENVIRONNEMENT</h1>
+    <?php
+    while($articlesEnvironnement->have_posts()){
+        $articlesEnvironnement->the_post();
+        ?>
+        <div class="article">
+            <a href=<?php the_permalink(); ?>><?php the_post_thumbnail('medium'); ?></a>
+            <div class="the-content">
+                <div class="<?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?>"> <?php echo get_the_category_list(', ') ?> </div>
+                <h2><a href=<?php the_permalink(); ?>> <?php the_title(); ?> </a></h2>
+                <div class = "extrait">
+                <?php
+                    if(has_excerpt()){
+                        the_excerpt();
+                    }else{
+                        echo wp_trim_words(get_the_content(), 25);
+                    }
+                ?>
+                </div>
+                <p class="temps"> <?php the_time('j F Y',) ?> </p>
+            </div>
+        </div>
+        <?php
+    } 
+          //-----------------------------------------------------------plus recents articles affaires
+          $articlesAffaires = new WP_Query(array(
+            'posts_per_page' => 3,
+            'orderby' => 'date',
+            'order' => 'DESC',
+            'category_name' => 'AFFAIRES'
+        ));
+        ?>
+        <h1>AFFAIRES</h1>
+        <?php
+        while($articlesAffaires->have_posts()){
+            $articlesAffaires->the_post();
+            ?>
+            <div class="article">
+                <a href=<?php the_permalink(); ?>><?php the_post_thumbnail('medium'); ?></a>
+                <div class="the-content">
+                    <div class="<?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?>"> <?php echo get_the_category_list(', ') ?> </div>
+                    <h2><a href=<?php the_permalink(); ?>> <?php the_title(); ?> </a></h2>
+                    <div class = "extrait">
+                    <?php
+                        if(has_excerpt()){
+                            the_excerpt();
+                        }else{
+                            echo wp_trim_words(get_the_content(), 25);
+                        }
+                    ?>
+                    </div>
+                    <p class="temps"> <?php the_time('j F Y',) ?> </p>
+                </div>
+            </div>
+            <?php
+        }
+             //-----------------------------------------------------------plus recents articles santé
+             $articlesSante = new WP_Query(array(
+                'posts_per_page' => 3,
+                'orderby' => 'date',
+                'order' => 'DESC',
+                'category_name' => 'SANTÉ'
+            ));
+            ?>
+            <h1>SANTÉ</h1>
+            <?php
+            while($articlesSante->have_posts()){
+                $articlesSante->the_post();
+                ?>
+                <div class="article">
+                    <a href=<?php the_permalink(); ?>><?php the_post_thumbnail('medium'); ?></a>
+                    <div class="the-content">
+                        <div class="<?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?>"> <?php echo get_the_category_list(', ') ?> </div>
+                        <h2><a href=<?php the_permalink(); ?>> <?php the_title(); ?> </a></h2>
+                        <div class = "extrait">
+                        <?php
+                            if(has_excerpt()){
+                                the_excerpt();
+                            }else{
+                                echo wp_trim_words(get_the_content(), 25);
+                            }
+                        ?>
+                        </div>
+                        <p class="temps"> <?php the_time('j F Y',) ?> </p>
+                    </div>
+                </div>
+                <?php
+            }
 ?>
 
 <?php 
--- themes/wp-la-presse/header.php
+++ themes/wp-la-presse/header.php
@@ -9,12 +9,21 @@
     <header>
         <div class="menu-burger"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path></svg></div>
         <h1><a href=<?php echo site_url(); ?>>La Presse</a></h1>
-        <div class="navigation menu-inactif">
-            <?php 
-            wp_nav_menu(array(
-                "theme_location" => "menuDuHeader"
-            ));
-            ?>
+        <div class="navigation menu-inactif">  
+            <div class="background-menu">
+                <div class="section-recherche">
+                    <input class="barre-recherche" type="search" placeholder = "Rechercher un article">
+                </div>
+                <div class="section-principale">
+                    <h3>SECTIONS PRINCIPALES</h3>
+                    <?php 
+                        wp_nav_menu(array(
+                            "theme_location" => "menuDuHeader"
+                        ));
+                    ?>
+                </div>
+
+            </div>
         </div>
         
     </header>
